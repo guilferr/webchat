790e383f041be2f1e08a3dc47ccf1312d76a2343
Guilherme Ferreira <guifcfarias@gmail.com>
2023-01-22 18:43:02 -0300

    feat(main): Create index file and use static files on server

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-import express from 'express'
+import express from 'express';
 
 const app = express()
 
+app.use(express.static('public'))
 app.get('/', (req: express.Request, res: express.Response) => {
-    res.status(200).json({
-        test: "test"
-    })
-})
+
+    res.sendFile(__dirname + '/index.html');
+
+});
 
 app.listen(3000, () => {
     console.log('Server is running in http://localhost:3000');
