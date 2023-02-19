7af4d7343fb2c6e23fff0c77554e27c682dc494e
Guilherme Ferreira <guifcfarias@gmail.com>
2023-02-19 11:57:54 -0300

    feat(main): Add form to enter new user

--- src/http.ts
+++ src/http.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 import http from 'http'
+import path from 'path'
 import { Server } from "socket.io"
 const app = express()
 const server = http.createServer(app)
 const io = new Server(server)
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, '..', 'public')))
 
 app.get('/', (req: express.Request, res: express.Response) => {
     res.sendFile(__dirname + '/index.html')
