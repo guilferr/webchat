f1b5d0d878964828c28bcbb748ce1bd6104b6085
Guilherme Ferreira <guifcfarias@gmail.com>
2023-02-21 19:05:00 -0300

    feat(main): Add body parser urlencoded to receive form params

--- src/http.ts
+++ src/http.ts
@@ -1,3 +1,4 @@
+import bodyParser from 'body-parser'
 import express from 'express'
 import http from 'http'
 import path from 'path'
@@ -7,9 +8,16 @@ const server = http.createServer(app)
 const io = new Server(server)
 
 app.use(express.static(path.join(__dirname, '..', 'public')))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req: express.Request, res: express.Response) => {
-    res.sendFile(__dirname + '/index.html')
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
+
+app.post('/chat', (req: express.Request, res: express.Response) => {
+    console.log(req.body);
+
+    res.sendFile(path.join(__dirname, '..', 'public', 'chat.html'))
 })
 
 export { server, io }
