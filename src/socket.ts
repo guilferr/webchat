95664cfa65e5c376992b57f5a8ef89517e2d5c07
Guilherme Ferreira <guifcfarias@gmail.com>
2023-02-25 19:20:43 -0300

    feat(main): Update socket when user reload

--- src/socket.ts
+++ src/socket.ts
@@ -2,11 +2,17 @@ import { io, users } from "./http"
 
 io.on('connection', (socket) => {
     const userWithoutId = users.find((user) => user.id === undefined)
+    const userInRoom = users.find((user) => userWithoutId.user === user.user && userWithoutId.room === user.room && user.id !== undefined)
+
+    if (userInRoom) {
+        userInRoom.id = socket.id
+        users.splice(users.indexOf(userWithoutId), 1)
+    }
+
     if (userWithoutId) {
         userWithoutId.id = socket.id
+        socket.join(userWithoutId.room)
     }
+
     console.log(users)
-    socket.on('disconnect', () => {
-        console.log('Usuário desconectado!')
-    })
 })
