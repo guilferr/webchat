7567cda2fe2510093bb1d305cb68ddc28b8b99af
Guilherme Ferreira <guifcfarias@gmail.com>
2023-03-13 20:05:57 -0300

    feat(main): Create emit message event when click submit

--- public/chat.js
+++ public/chat.js
@@ -2,9 +2,10 @@
 const socket = io()
 
 const inputMessage = document.getElementById('message')
+const buttonMessage = document.getElementById('sendMessage')
 const user = document.getElementById('user').textContent
 const room = document.getElementById('room').attributes.name.value
-console.log(room)
+
 inputMessage.addEventListener("keypress", (event) => {
     if (event.key === "Enter") {
         const message = event.target.value
@@ -15,5 +16,20 @@ inputMessage.addEventListener("keypress", (event) => {
         }
 
         socket.emit("message", data)
+
+        event.target.value = ""
     }
+})
+
+buttonMessage.addEventListener("click", (event) => {
+    const message = inputMessage.value
+    const data = {
+        message,
+        user,
+        room
+    }
+
+    socket.emit("message", data)
+
+    inputMessage.value = ""
 })
